e55b45e3b3ffcf57e99345bcbf7dcad87c309dd8
li1553770945 <1553770945@qq.com>
2024-01-27 21:21:33 +0800

    init:新建项目

---
+++ mqtt.py
@@ -0,0 +1,13 @@
+class MQTT:
+    def __init__(self,addr:str,port:int,username:str,passwd:str) -> None:
+        self.addr = addr
+        self.port = port
+        self.username = username
+        self.passwd = passwd
+        pass
+
+    def connect(self):
+
+        pass
+
+    
